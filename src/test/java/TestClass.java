aff10d767d4e6006167cb5e146c4d084500dec81
Ben Frey <spartanhooah@gmail.com>
2023-08-01 12:46:10 -0400

    add pitest

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TestClass {
+    @Test
+    void helloJunit() {
+        assertTrue(2 == 2);
+    }
 }
